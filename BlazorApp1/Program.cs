bcff860ec494678e564351dece05aecdb2986966
Yohan Fairfort <yohan.fairfort@gmail.com>
2023-09-26 22:09:05 +0200

    Put initialise in the program instead of the page

--- BlazorApp1/Program.cs
+++ BlazorApp1/Program.cs
@@ -6,7 +6,14 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<CatMash>();
+builder.Services.AddSingleton<CatMash>(provider =>
+{
+    var catMashApi = provider.GetRequiredService<ICatMashApi>();
+    var catRepository = provider.GetRequiredService<ICatRepository>();
+    var catMash = new CatMash(catRepository, catMashApi);
+    Task.Run(async () => await catMash.Initialise());
+    return catMash;
+});
 builder.Services.AddSingleton<ICatRepository, InMemoryCatRepository>();
 builder.Services.AddHttpClient<ICatMashApi, CatMashApi>();
 
